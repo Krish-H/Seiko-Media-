b01212eb31081b7374f6621b05ba1d4167d65ead
krish <krishhh192502@gmail.com>
2024-08-30 14:18:10 +0530

    commit

--- src/components/Our.jsx
+++ src/components/Our.jsx
@@ -32,13 +32,12 @@ const Our = () => {
   }, []);
 
   return (
-    <div className="w-full gap-12 h-screen flex flex-col md:flex-row items-center justify-center p-8 pt-[12rem] pb-[8rem]   bg-gray-100">
+    <div className="w-full h-screen flex flex-col md:flex-row items-center justify-center p-8 pt-[5rem] md:pt-[8rem] pb-[8rem] bg-gray-100">
       <div
         ref={missionRef}
-        className="w-full md:w-1/2 h-full flex flex-col items-center justify-center text-center md:text-justify px-4"
-        style={{ marginBottom: 0, paddingBottom: 0 }}
+        className="w-full md:w-1/2 flex flex-col items-center justify-center text-center md:text-left px-4 mb-12 md:mb-0"
       >
-        <img src={assets.target} alt="Mission" className="w-1/4 h-auto mb-4 " />
+        <img src={assets.target} alt="Mission" className="w-1/4 h-auto mb-4" />
         <h1 className="text-3xl md:text-4xl font-bold text-[#1E2A38] mb-4">Our Mission</h1>
         <p className="text-lg text-gray-700 leading-relaxed">
           At Seiko Media, our mission is simple: to build the best products that create the most value for our clients. We are driven by innovation, creativity, and data-driven decisions, ensuring that we deliver exceptional outcomes that exceed expectations.
@@ -47,10 +46,9 @@ const Our = () => {
 
       <div
         ref={visionRef}
-        className="w-full md:w-1/2 h-full flex flex-col items-center justify-center text-center md:text-justify px-4"
-        style={{ marginBottom: 0, paddingBottom: 0 }}
+        className="w-full md:w-1/2 flex flex-col items-center justify-center text-center md:text-left px-4"
       >
-        <img src={assets.telescope} alt="Vision" className="w-1/4 h-auto mb-4 " />
+        <img src={assets.telescope} alt="Vision" className="w-1/4 h-auto mb-4" />
         <h1 className="text-3xl md:text-4xl font-bold text-[#1E2A38] mb-4">Our Vision</h1>
         <p className="text-lg text-gray-700 leading-relaxed">
           We strive to go above and beyond for our clients, tackling challenges head-on and delivering our best work every single day. Our vision is to help brands achieve exponential growth, month-on-month, year-on-year.
